fcf8261cb7cb8f777f2d5bc4d28a100dad068d31
RastkoSavic <rastkosavic23@gmail.com>
2018-04-12 02:13:40 +0200

    Formatted Files

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
-
 const hbs = require('hbs');
-
 const fs = require('fs');
 
 const port = process.env.PORT || 3000;
@@ -14,13 +12,15 @@ app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
     var now = new Date().toString();
-    var log = `${now}: ${req.method} ${req.url}`
+    var log = `${now}: ${req.method} ${req.url}`;
     console.log(log);
+
     fs.appendFile('server.log', log + '\n', (err) => {
         if (err) {
             console.log('Unable to append to server.log.');
         }
     });
+
     next();
 });
 
